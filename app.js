3cfd8981e943351c4073c4275b3a5e582d82160d
Отгоо <jf15017583402@gmail.com>
2021-05-08 19:55:15 +0800

    Orlogo zardaliig avah utga butetsiig burtgev

--- app.js
+++ app.js
@@ -1,10 +1,49 @@
-var uiController = (function () {})();
+var uiController = (function () {
+  var DOMstrings = {
+    input: ".add__type",
+    inputDescription: ".add__description",
+    inputValue: ".add__value",
+    addBtn: ".add__btn",
+  };
+  return {
+    getInput() {
+      return {
+        type: document.querySelector(DOMstrings.input).value,
+        description: document.querySelector(DOMstrings.inputDescription).value,
+        value: document.querySelector(DOMstrings.inputValue).value,
+      };
+    },
+    getDomstrings: function () {
+      return DOMstrings;
+      ("");
+    },
+  };
+})();
 
 var financeController = (function () {})();
-
+var Income = function (id, description, value) {
+  this.id = id;
+  this.description = description;
+  this.value = value;
+};
+var Expense = function (id, description, value) {
+  this.id = id;
+  this.description = description;
+  this.value = value;
+};
+var data = {
+  allItems: {
+    inc: [],
+    exp: [],
+  },
+  total: {
+    inc: 1500,
+    exp: 0,
+  },
+};
 var appController = (function (uiController, financeController) {
   var ctrlAddItem = function () {
-    console.log("tovch dargdlaa");
+    console.log(uiController.getInput());
   };
   // 1/'oruulah uguduluudiig delgetsnees olj avna
 
@@ -13,12 +52,23 @@ var appController = (function (uiController, financeController) {
   //4/tusuviin tootsoog gargana
   //5/etssiin uldegdel tootsgg urdeng deggestend gargana
 
-  document.querySelector(".add__btn").addEventListener("click", function () {
-    ctrlAddItem();
-  });
-  document.addEventListener("keypress", function (event) {
-    if (event.keyCode === 13 || event.which === 13) {
+  var setUpEventListener = function () {
+    var DOM = uiController.getDomstrings();
+
+    document.querySelector(DOM.addBtn).addEventListener("click", function () {
       ctrlAddItem();
-    }
-  });
+    });
+    document.addEventListener("keypress", function (event) {
+      if (event.keyCode === 13 || event.which === 13) {
+        ctrlAddItem();
+      }
+    });
+  };
+  return {
+    init: function () {
+      console.log(" Programm Ehellee");
+      setUpEventListener();
+    },
+  };
 })(uiController, financeController);
+appController.init();
